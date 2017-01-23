7c74e35c4c903448c5440a2b0e095402b18fe27a
Katie Mercurio <katiemercurio@dhcp-wifi-8021x-155-41-74-229.bu.edu>
2017-01-23 15:13:37 -0500

    initial commit - r-intro

---
+++ rintrochunks.R
@@ -0,0 +1,5 @@
+1.4 R and the window system
+The most convenient way to use R is at a graphics workstation running a windowing system.
+This guide is aimed at users who have this facility. In particular we will occasionally refer to
+the use of R on an X window system although the vast bulk of what is said applies generally to
+any implementation of the R environment.
