006d35f17b7c3d9312bb314843613376b5b4f58d
Katie Mercurio <katiemercurio@dhcp-wifi-8021x-155-41-74-229.bu.edu>
2017-01-23 15:16:31 -0500

    second chunk added

--- rintrochunks.R
+++ rintrochunks.R
@@ -3,3 +3,10 @@ The most convenient way to use R is at a graphics workstation running a windowin
 This guide is aimed at users who have this facility. In particular we will occasionally refer to
 the use of R on an X window system although the vast bulk of what is said applies generally to
 any implementation of the R environment.
+
+In using R under UNIX the suggested procedure for the first occasion is as follows:
+  1. Create a separate sub-directory, say work, to hold data files on which you will use R for
+this problem. This will be the working directory whenever you use R for this particular
+problem.
+$ mkdir work
+$ cd work
